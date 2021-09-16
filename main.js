6725b4c6aa090b04e2527c1b889a0a0451f8630e
oleksay2006 <oleksay2006@gmail.com>
2021-09-16 23:49:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// let height = (screen.width+'x'+screen.height);
+// console.log(height);
+// let header = document.querySelector('header');
+// let navigation = document.querySelector('nav');
+// let sum = header.height + navigation.height;
+// console.log(sum);
