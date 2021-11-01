edcd2a25e60b21c9f950e4a889d902e1d20e89db
oleksay2006 <oleksay2006@gmail.com>
2021-11-01 23:51:36 +0200

    third deploy

---
+++ dist/precache-manifest.c534098c6f522bf212cf59465ccbb8a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2b63aedeff45931e85",
+    "url": "/onix-test-site/css/app.d63394e3.css"
+  },
+  {
+    "revision": "0e6e01089d4327013cb51feaf39e56ab",
+    "url": "/onix-test-site/img/Logo@3x.0e6e0108.svg"
+  },
+  {
+    "revision": "0f37f895d23de83b909154b2f544ca13",
+    "url": "/onix-test-site/img/beach-2.0f37f895.jpg"
+  },
+  {
+    "revision": "1ee09e464732c2d40fd95a07d62ccb74",
+    "url": "/onix-test-site/img/beach.1ee09e46.jpg"
+  },
+  {
+    "revision": "71c9b768d39f416408645075d39af3c6",
+    "url": "/onix-test-site/img/fiord.71c9b768.jpg"
+  },
+  {
+    "revision": "cd3014db9f7a85ab58c79bd568b460da",
+    "url": "/onix-test-site/img/profile.cd3014db.svg"
+  },
+  {
+    "revision": "663f17ff118e7ca71a60d0c580877938",
+    "url": "/onix-test-site/img/town.663f17ff.jpg"
+  },
+  {
+    "revision": "b00c67c004e2ba2403ffb585a4843ba9",
+    "url": "/onix-test-site/index.html"
+  },
+  {
+    "revision": "ac2b63aedeff45931e85",
+    "url": "/onix-test-site/js/app.1a255eeb.js"
+  },
+  {
+    "revision": "77f32bda97f07aa75778",
+    "url": "/onix-test-site/js/chunk-vendors.3264cbf7.js"
+  },
+  {
+    "revision": "4ba9e09663be3a1f81b3be13fa116e4b",
+    "url": "/onix-test-site/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/onix-test-site/robots.txt"
+  }
+]);
